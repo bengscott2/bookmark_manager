6b839f1938399778e119616bc3f5e01d05d14769
Ben Scott <bengscott2@gmail.com>
2019-06-17 16:17:00 +0100

    added conection to bookmark DB

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,11 @@
+require 'pg'
+
 class Bookmark
 
   def self.all
-    ["http://www.google.com",
-      "http://www.bing.com",
-      "http://www.yahoo.com"]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,8 +4,8 @@ describe Bookmark do
   describe '#bookmarks' do
     it 'returns a list of bookmarks' do
       bookmarks = Bookmark.all
-      expect(bookmarks).to include("http://www.yahoo.com")
-      expect(bookmarks).to include("http://www.bing.com")
+      expect(bookmarks).to include("http://www.makersacademy.com")
+      expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")
      end
   end
