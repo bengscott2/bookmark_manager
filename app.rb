c588d816ed30b179a5d3c64abb46b23468c71ab3
Ben Scott <bengscott2@gmail.com>
2019-06-19 16:18:11 +0100

    create a delete function

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra/base'
 require_relative './lib/bookmark.rb'
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions, :method_override
+
   get '/' do
     "Bookmark Manager"
   end
@@ -19,5 +22,11 @@ class BookmarkManager < Sinatra::Base
     Bookmark.create(url: params[:url], title: params[:title])
     redirect :bookmarks
   end
+
+  delete '/bookmarks/:id' do
+    Bookmark.delete(id: params[:id])
+    redirect :bookmarks
+  end
+
   run! if app_file == $0
 end
---
+++ spec/features/deleting_a_bookmark_spec.rb
@@ -0,0 +1,10 @@
+feature 'deleting a bookmark' do
+  scenario 'should delete a certain bookmark when you click delete button' do
+    Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit('/bookmarks')
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    first('.bookmark').click_button 'Delete'
+    expect(current_path).to eq '/bookmarks'
+    expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+  end
+end
